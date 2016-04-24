eb77f5610b036ddde273fde944a9514672f592db
mikelsanz95 <mikelsanz95@opendeusto.es>
2016-04-24 20:13:49 +0200

    Main.c

--- MonsterCave/main.c
+++ MonsterCave/main.c
@@ -157,50 +157,54 @@ void nuevasala( Player* p, Sala* s, Monstruo* m)
 			int i=0;
 			for(;i<1;i++ )//OJOOOO EL  con el numero de monstruos en el array esta en 1 para probar
 			{
-				printf("¿Peta aqui? \n");
-				fflush(stdout);
+
+
 				if(s[numSala].codSala == m[i].codsala)
 				{
-					printf("¿Peta aqui? \n");
-					fflush(stdout);
+
+
 					break;
 				}
 			}
-			printf("¿Peta aqui? \n");
-			fflush(stdout);
+
+
 			int vida = m[i].vida;
 
-			printf("%s  \n" , m[i].interacciones[0]); // <---------------------- AQUI PETA ------------------------------------------------------------
-			while(vida>=0)
+			printf("%s  \n" , m[i].interacciones[0]);
+			while(vida>0)
 			{
 
 				if(p->vida<=0)//Game over
 				{
-					printf("%s /n   Pulsa cualquier tecla para continuar", s[numSala].textosDeSala[2]);// Texto de game over
+					printf("%s /n   Pulsa cualquier tecla para continuar", s[numSala].textosDeSala[2]);
+					fflush(stdout);// Texto de game over
 					char m;
 					m =getchar();
 					break;
 
 				}
 				//Print de la vida
-				printf("Vida del monstruo : ");
+				printf("%s \n", m[i].nombre);
+				printf("Vida del monstruo :");
 				fflush(stdout);
-				for(;i<vida;i++)
+				int j=0;
+				for(;j<vida;j++)
 				{
 					printf("|");
 					fflush(stdout);
 				}
+				printf("\n%s:",p->name);
 
-				printf("/n Escoge un arma (1/2/3/4)");
-				printf("/n 1 - Espada de palo      2 - Piro++      \n 3-Hielo++          4 - Cura++       ");
-
-				int k;
-				printf("Tu vida :");
+				int k=0;
 				for(;k<p->vida;k++)
 				{
 					printf("|");
 					fflush(stdout);
 				}
+
+				printf("\n 1 - Espada de palo      2 - Piro++      \n 3-Hielo++          4 - Cura++       \n");
+				printf("\n Escoge un arma (1/2/3/4) : ");
+				fflush(stdout);
 				int eleccion = 0;
 				scanf("%i", &eleccion);
 				fflush(stdin);
@@ -209,13 +213,23 @@ void nuevasala( Player* p, Sala* s, Monstruo* m)
 				case 1:
 
 					vida=vida-20;
+					printf("Dañas al monstruo con tu legendaria espadad de madera ");
+					fflush(stdout);
+					sleep(3);
 					p->vida = p->vida - 10;
+					printf("Sin embargo se astilla y recibes daño ");
+					fflush(stdout);
+
 
 					break;
+
 				case 2:
 					vida=vida-15;
+					printf("Conjuras una gran llama de fuego, el animal daña al monstruo, ");
+					fflush(stdout);
 					p->vida = p->vida - 7;
-
+					printf("Sin embargo se astilla y recibes daño ");
+					sleep(3);
 					break;
 				case 3:
 					vida=vida-5;
@@ -239,30 +253,29 @@ void nuevasala( Player* p, Sala* s, Monstruo* m)
 
 
 
-
-				//Espera una respuesta
-
-				//Se le quita vida y sale respuesta de monstruo de ay me has hecho ¿daño?
-
-
-				//¿esperar un tiempo?
-
 			}
 
 
 			//texto de salida de la
-			printf("Sala %d  \n %s", numSala + 1, s->textosDeSala[2]);
+			printf("\n%s", s->textosDeSala[2]);
+			fflush(stdout);
 		}
-//		else if (s[numSala].tipo == 2)// sala en la que consigues algo
-//
-//		{
-//			printf("Sala %d  \n %s", numSala + 1, s->textosDeSala[0]);
-//
-//			//sala en la que te encuentras con un hechizo random?
-//
-//
-//
-//		}
+		else if (s[numSala].tipo == 2)// sala en la que consigues algo
+
+		{
+			printf("Sala %d  \n %s", numSala + 1, s->textosDeSala[0]);
+			fflush(stdout);
+			p->vida=100;
+			int k=0;
+			for(;k<p->vida;k++)
+			{
+					printf("|");
+					fflush(stdout);
+					sleep(0.005);
+			}
+
+
+		}
 
 
 
